f9964b31acd23ca2e58584c17619e3aaad455d77
Refaldi Julidinsyah <syahrefaldi@gmail.com>
2026-06-14 21:21:53 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-amber-100">
+      <h1 className="text-4xl font-bold text-amber-600 drop-shadow-md">
+        Tailwind v4 Setup Sukses! 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
